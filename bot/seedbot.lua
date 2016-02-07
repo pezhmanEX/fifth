4b56336b0a43646d2d4a072962f8a24bd736ad5d
pezhmanEX <pezhmannoormohammadi@gmail.com>
2016-02-07 21:32:30 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -224,7 +224,8 @@ function create_config( )
     "invite",
     "all",
     "leave_ban",
-    "admin"
+    "admin",
+    "ids"
     },
     sudo_users = {123649999,tonumber(our_id)},--Sudo users
     disabled_channels = {},
